8539bb8a739e7b5f493d5b21073ec502bdfa51e9
Wendy <lienyun.chang@gmail.com>
2022-05-19 10:40:50 +0800

    controllers

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+const getLogin = (req, res) => {
+    res.status(200)
+        .render('login', {
+            path: '/login',
+            pageTitle: 'Login'
+        });
+};
+
+const postLogin = (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+        res.redirect('/');
+    } else {
+        console.log('欄位尚未填寫完成！')
+    }
+};
+
+const postLogout = (req, res) => {
+    res.redirect('/login')
+};
+
+module.exports = {
+    getLogin,
+    postLogin,
+    postLogout,
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,17 @@
 const express = require('express');
 
+const authController = require('../controllers/auth')
+
 ////////////////////////////////////////////////////////////
-//把原本貼在app.js的app.get, post貼過來，並且把app改成router
+
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.status(200)
-        .render('login', {
-            path: '/login',
-            pageTitle: 'Login'
-        });
-});
+router.get('/login', authController.getLogin);
 
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    if (email && password) {
-        res.redirect('/');
-    } else {
-        console.log('欄位尚未填寫完成！')
-    }
-});
+router.post('/login', authController.getLogin);
 
-router.post('/logout', (req, res) => {
-    // TODO: 實作 logout 機制
-    res.redirect('/login')
-});
+router.post('/logout', authController.postLogout);
 
 //輸出router
 module.exports = router;
