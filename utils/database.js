391f7994ffd0da41762b4aa7597d80e973f9dbaf
shin.kuo15 <shin.kuo15@gmail.com>
2022-06-10 15:46:19 +0800

    測試

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 ////////////////////////////////////////////////////////////
 
-const database = new Sequelize('vue-node-sports', 'root', '403200281', {
+const database = new Sequelize('jml-all', 'root', 'fn102', {
     dialect: 'mysql', 
     host: 'localhost'
 });
